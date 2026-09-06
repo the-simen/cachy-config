46e203144564b55e8d593712ed67dbf2d4558f13
Tonkikh Semen <tonksemen@gmail.com>
2026-09-06 15:23:24 +0300

    upd

--- tmux/scripts/git-branch.sh
+++ tmux/scripts/git-branch.sh
@@ -4,5 +4,5 @@ branch=$(git -C "$1" branch --show-current 2>/dev/null)
 
 if [ -n "$branch" ]; then
     printf '%s' \
-        '#[fg=#{E:@surf-br},bg=#{E:@t-green}]#[fg=black,bg=#{E:@t-green}] 󰘬 '"$branch"' #[fg=#{E:@surf-br},bg=#{E:@t-green}]#{E:@segment-separator}'
+        '#[fg=#{E:@surf-br},bg=#{E:@t-green}]#[fg=#{E:@noctalia_scrim},bg=#{E:@t-green}] 󰘬 '"$branch"' #[fg=#{E:@surf-br},bg=#{E:@t-green}]#{E:@segment-separator}'
 fi
